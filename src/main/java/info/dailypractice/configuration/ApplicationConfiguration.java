bc3a3405b027665450febd9695408ab67adf7c0f
initial@gmail.com <teamdailypractice@gmail.com>
2024-09-11 07:42:42 +0530

    ObjectMapper can be Singleton - no need of new object everytime

--- src/main/java/info/dailypractice/configuration/ApplicationConfiguration.java
+++ src/main/java/info/dailypractice/configuration/ApplicationConfiguration.java
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import info.dailypractice.pdf.Book;
 import info.dailypractice.pdf.BookPage;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class ApplicationConfiguration {
 
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public ObjectMapper objectMapper(){
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new Jdk8Module());
