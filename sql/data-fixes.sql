2a84d1114e8fd8bc1d29c7e74b77269e4b08df41
initial@gmail.com <teamdailypractice@gmail.com>
2024-09-26 21:50:55 +0530

    author 2 - data fix applied to db

--- sql/data-fixes.sql
+++ sql/data-fixes.sql
@@ -14,3 +14,15 @@ where author_id = 2 and urai like '%@@@@@%'
 UPDATE thirukkural_author_urai
 SET urai = REPLACE(urai, '@@@@@','')
 where author_id = 2 and urai like '%@@@@@%'
+
+--author_id = 2 -- data fixes applied
+SELECT *
+FROM
+thirukkural_author_urai
+WHERE author_id = 2
+and urai like '%`%'
+
+UPDATE thirukkural_author_urai
+SET urai = REPLACE(urai,'`','')
+WHERE author_id = 2
+and urai like '%`%'
